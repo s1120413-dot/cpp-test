10594d916a854a1356453c7cd930710e65b2a23b
s1120413-dot <s1120413@dmhs.kh.edu.tw>
2025-10-09 13:42:37 +0800

    作業4

--- main.cpp
+++ main.cpp
@@ -1,17 +1,11 @@
-int main() {
-   
-    int score1, score2, score3;
-    cout << "請輸入3次分數(0~100): ";
-    cin >> score1 >> score2 >> score3;
-    double averageScore = (score1 + score2 + score3)/3;
-    if (score1 < 40 || score2 < 40 || score3 < 40) {
-        cout << "不及格\n";
-    } else if (averageScore >= 60) {
-        cout << "你及格\n";
-    } else if (averageScore < 60) {
-        cout << "不及格\n";
-    }
-    cout << "你的平均為: \a" << averageScore;
+#include <iostream>
+using namespace std;
 
+int main() {
+    for (int i = 1; i <= 50; i++) {
+        if (i % 7 == 0) {
+            cout << i << " ";
+        }
+    }
     return 0;
 }
