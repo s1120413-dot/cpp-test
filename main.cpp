1e67c72338b4d2340e1368f6aeca491e2db8a72d
s1120413-dot <s1120413@dmhs.kh.edu.tw>
2025-10-16 14:36:05 +0800

    hw5
    Added functionality to print asterisks in a pyramid shape and a multiplication table.

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 using namespace std;
 
 int main() {
-    for (int i = 1; i <= 50; i++) {
-        if (i % 7 == 0) {
-            cout << i << " ";
+    // 第一部分：印出星號金字塔
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= i; ++j) {
+            cout << "*";
         }
+        cout << endl;
     }
+
+    cout << endl;  // 分隔兩部分
+
+    // 第二部分：印出九九乘法表
+    for (int i = 1; i <= 9; ++i) {
+        for (int j = 1; j <= 9; ++j) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
